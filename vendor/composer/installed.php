b0cb44617b0cd832e4eb39d27f16545c7a2ae9fb
dzikri-dotcom <email@dzikrilathifulqodim.com>
2025-01-01 18:11:44 +0700

    Dz

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '83c11f6901c4f4f09a27e71be08ab779767a2afa',
+        'reference' => 'cae2567e3f8e78914fdd4f80ffb96140e110e5db',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -400,7 +400,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '83c11f6901c4f4f09a27e71be08ab779767a2afa',
+            'reference' => 'cae2567e3f8e78914fdd4f80ffb96140e110e5db',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -820,8 +820,8 @@
         'psr/log-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '1.0|2.0|3.0',
-                1 => '3.0.0',
+                0 => '3.0.0',
+                1 => '1.0|2.0|3.0',
             ),
         ),
         'psr/simple-cache' => array(
