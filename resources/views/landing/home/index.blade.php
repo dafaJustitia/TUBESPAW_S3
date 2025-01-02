71911eb5a79d3d6adb3ff272cedc00547ef0b8aa
dzikri-dotcom <dzikrilathifulqodim@gmail.com>
2025-01-02 19:22:51 +0700

    footer

--- resources/views/landing/home/index.blade.php
+++ resources/views/landing/home/index.blade.php
@@ -181,17 +181,135 @@
         </article>
     </main>
 
-    <!-- Footer -->
+    <!--
+      - #FOOTER
+    -->
+
     <footer class="footer">
         <div class="container">
+
             <div class="footer-top">
-                <!-- Footer content -->
+
+                <div class="footer-brand">
+                    <a href="#" class="logo">
+                        <img src="{{ asset('img/logo.png') }}" alt="logo" style="width: 50%;">
+                    </a>
+
+                    <p class="footer-text">
+                        Ruang Obat adalah platform yang menyediakan berbagai macam obat dengan harga terjangkau.
+                    </p>
+                </div>
+
+                <ul class="footer-list">
+
+                    <li>
+                        <p class="footer-list-title">Perusahaan</p>
+                    </li>
+
+                    <li>
+                        <a href="#" class="footer-link">Tentang Kami</a>
+                    </li>
+
+                    <li>
+                        <a href="#" class="footer-link">Kontak Kami</a>
+                    </li>
+
+                </ul>
+
+                <ul class="footer-list">
+
+                    <li>
+                        <p class="footer-list-title">Support</p>
+                    </li>
+
+                    <li>
+                        <a href="#" class="footer-link">Pusat Bantuan</a>
+                    </li>
+
+                    <li>
+                        <a href="#" class="footer-link">Pertanyaan ? </a>
+                    </li>
+
+                    <li>
+                        <a href="#" class="footer-link">Privacy policy</a>
+                    </li>
+
+                    <li>
+                        <a href="#" class="footer-link">Terms & conditions</a>
+                    </li>
+
+                </ul>
+
+                <ul class="footer-list">
+
+                    <li>
+                        <p class="footer-list-title">Regional</p>
+                    </li>
+
+                    <li>
+                        <a href="#" class="footer-link">Jawa Timur</a>
+                    </li>
+
+                    <li>
+                        <a href="#" class="footer-link">Jawa Tengah</a>
+                    </li>
+
+                    <li>
+                        <a href="#" class="footer-link">Jawa Barat</a>
+                    </li>
+
+                </ul>
+
             </div>
+
             <div class="footer-bottom">
+
+                <ul class="social-list">
+
+                    <li>
+                        <a href="#" class="social-link">
+                            <ion-icon name="logo-facebook"></ion-icon>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#" class="social-link">
+                            <ion-icon name="logo-instagram"></ion-icon>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#" class="social-link">
+                            <ion-icon name="logo-twitter"></ion-icon>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#" class="social-link">
+                            <ion-icon name="logo-linkedin"></ion-icon>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#" class="social-link">
+                            <ion-icon name="logo-skype"></ion-icon>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#" class="social-link">
+                            <ion-icon name="mail-outline"></ion-icon>
+                        </a>
+                    </li>
+
+                </ul>
+
                 <p class="copyright">
                     &copy; 2024 <a href="#">Ruang Obat</a>. All Rights Reserved
                 </p>
+
             </div>
+
         </div>
     </footer>
 
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => 'dev-master',
-        'version' => 'dev-master',
-        'reference' => 'b4d41673caa424524c8287615c19a7a000dc1093',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -398,9 +398,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => 'dev-master',
-            'version' => 'dev-master',
-            'reference' => 'b4d41673caa424524c8287615c19a7a000dc1093',
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
