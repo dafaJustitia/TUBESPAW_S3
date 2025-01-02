b9af53e6014010137ca0c22db0d4b3ebee7c6ae1
dafaJustitia <dafajustitia05@gmail.com>
2025-01-02 12:42:20 +0700

    user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,18 +15,37 @@ class UserSeeder extends Seeder
     {
         User::insert([
             [
-                'name' => 'Devies',
-                'email' => 'deviesade@gmail.com',
-                'password' => bcrypt('123456'),
+                'name' => 'dafa',
+                'email' => 'dafa@gmail.com',
+                'password' => bcrypt('dafa'),
                 'avatar_url' => null
             ],
+            [
+                'name' => 'ayod',
+                'email' => 'ayod@gmail.com',
+                'password' => bcrypt('ayod'),
+                'avatar_url' => null
+            ],
+            [
+                'name' => 'alvyn',
+                'email' => 'alvyn@gmail.com',
+                'password' => bcrypt('alvyn'),
+                'avatar_url' => null
+            ],
+            [
+                'name' => 'dzikri',
+                'email' => 'dzikri@gmail.com',
+                'password' => bcrypt('dzikri'),
+                'avatar_url' => null
+            ]
+            ,
             [
                 'name' => 'Administator',
                 'email' => 'admin@gmail.com',
                 'password' => bcrypt('123456'),
                 'avatar_url' => null
             ]
-            
+
         ]);
     }
 }
